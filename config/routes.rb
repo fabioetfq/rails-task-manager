a0089f94f29d07419ced8e73708c733977247308
Fabio Oliveira <fabio.oliveira@agricultura.gov.br>
2021-10-20 18:57:45 -0300

    mínimo produto viável

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,7 @@ Rails.application.routes.draw do
   # UPDATE
   get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
   patch 'tasks/:id', to: 'tasks#update'
+
+  # DESTROY
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
