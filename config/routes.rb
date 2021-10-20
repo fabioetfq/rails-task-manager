62974aa23d7856561716d433af23edfd1df8437f
Fabio Oliveira <fabio.oliveira@agricultura.gov.br>
2021-10-20 17:12:31 -0300

    pausa para instalar extensões

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  # INDEX
+  get 'index', to: 'tasks#index'
+
+  # SHOW
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  
+  # CREATE
+  get 'new', to: 'tasks#new', as: :new_task
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
