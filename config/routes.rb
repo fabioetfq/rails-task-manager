80e8aec3b71670066c7d2afce43f7da47eb4aa33
Fabio Oliveira <fabio.oliveira@agricultura.gov.br>
2021-10-20 18:36:02 -0300

    get post patch

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # INDEX
-  get 'index', to: 'tasks#index'
+  get 'tasks', to: 'tasks#index'
+
+  # CREATE
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create'
 
   # SHOW
   get 'tasks/:id', to: 'tasks#show', as: :task
-  
-  # CREATE
-  get 'new', to: 'tasks#new', as: :new_task
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # UPDATE
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
 end
